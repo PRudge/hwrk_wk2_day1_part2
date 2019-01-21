cd9e18e18d6142e670519b78f8b3353167c9cdba
Pauline Rudge <paulinerudge@me.com>
2019-01-21 13:42:08 +0000

    used long hand methods for getters and setters

---
+++ Team.rb
@@ -0,0 +1,24 @@
+class SportsTeam
+  # attr_accessor :name, :players, :coach
+
+  # need an initialize to grab the input to the class
+  def initialize(input_name, input_players, input_coach) # initialize is a ruby
+    @name = input_name
+    @players = input_players
+    @coach = input_coach
+  end
+
+
+  def get_name()
+    return @name
+  end
+
+  def get_players()
+    return @players
+  end
+
+  def get_coach()
+    return @coach
+  end
+
+end
---
+++ specs/Team_spec.rb
@@ -0,0 +1,36 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../Team')
+
+class TestTeam < MiniTest::Test
+
+  # Now we would like you to make a class that represents a sports team.
+  #
+  # Make a class to represent a Team that has the properties Team name (String), Players (array of strings) and a Coach (String).
+  # For each property in the class make a getter method than can return them.
+  # Create a setter method to allow the coach's name to be updated.
+  # Refactor the class to use attr_reader or attr_accessor instead of your own getter and setter methods.
+  # Create a method that adds a new player to the players array.
+  # Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+  # Add a points property into your class that starts at 0.
+  # Create a method that takes in whether the team has won or lost and updates the points property for a win.
+
+  def setup
+    # create an object
+    @team_info = SportsTeam.new("Celtic",["Daniel Rudge", "Maddy Rudge", "Brogan Rudge", "Harry Bamber"], "Pauline Rudge")
+  end
+
+  def test_team_name
+    # assert_equal("Celtic", @team_info.name())
+    assert_equal("Celtic", @team_info.get_name())
+  end
+
+  def test_players
+    # assert_equal(["Daniel Rudge", "Maddy Rudge", "Brogan Rudge", "Harry Bamber"], @team_info.players())
+    assert_equal(["Daniel Rudge", "Maddy Rudge", "Brogan Rudge", "Harry Bamber"], @team_info.get_players())
+  end
+  # #
+  # def test_account_type
+  #   assert_equal("Pauline Rudge", @team_info.coach())
+  # end
+end
